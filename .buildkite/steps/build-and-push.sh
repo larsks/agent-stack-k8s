deffc0a02b7bc6c69d1706e84b9e2790de51caaa
Josh Deprez <jd@buildkite.com>
2024-12-11 16:05:56 +1100

    Push images to Public ECR

--- .buildkite/steps/build-and-push.sh
+++ .buildkite/steps/build-and-push.sh
@@ -3,30 +3,49 @@
 set -eufo pipefail
 
 echo --- :hammer: Installing tools
-apk add --update-cache --no-progress helm yq skopeo git
+apk add --update-cache --no-progress helm yq skopeo git aws-cli
 
 echo --- :git::docker: determining version and tags
 source .buildkite/steps/repo_info.sh
 
-echo --- :docker: Logging into ghcr.io
-skopeo login ghcr.io \
-  -u "${REGISTRY_USERNAME}" \
-  --password "${REGISTRY_PASSWORD}" \
-  --authfile ~/.docker/config.json
-
-echo --- :helm: Packaging helm chart
-yq -i ".image = \"${controller_image}\"" charts/agent-stack-k8s/values.yaml
+# TODO: Once the agent is also pushed to PECR, specialise this per registry
 yq -i ".config.image = \"${agent_image}\"" charts/agent-stack-k8s/values.yaml
-helm package charts/agent-stack-k8s --app-version "${version}" -d dist --version "${version}"
 
-echo --- :helm: Pushing helm chart to ghcr.io
-helm push "dist/agent-stack-k8s-${version}.tgz" "${helm_repo}"
+if [[ "${helm_repo_pecr:-}" != "" ]] ; then
+    source .buildkite/steps/assume-role.sh
+
+    echo "~~~ :helm: Logging into Public ECR"
+    helm login public.ecr.aws -u AWS --password "$(aws --region us-east-1 ecr-public get-login-password)"
+
+    echo "--- :helm: Packaging helm chart for Public ECR"
+    yq -i ".image = \"${controller_image_pecr}\"" charts/agent-stack-k8s/values.yaml
+    helm package charts/agent-stack-k8s --app-version "${version}" -d dist --version "${version}"
+
+    echo "--- :helm: Pushing helm chart to public.ecr.aws"
+    helm push "dist/agent-stack-k8s-${version}.tgz" "${helm_repo_pecr}"
+fi
+
+if [[ "${helm_repo_ghcr:-}" != "" ]] ; then
+    echo "--- :docker: Logging into ghcr.io"
+    skopeo login ghcr.io \
+        -u "${REGISTRY_USERNAME}" \
+        --password "${REGISTRY_PASSWORD}" \
+        --authfile ~/.docker/config.json
+
+    echo "--- :helm: Packaging helm chart for GHCR"
+    yq -i ".image = \"${controller_image_ghcr}\"" charts/agent-stack-k8s/values.yaml
+    helm package charts/agent-stack-k8s --app-version "${version}" -d dist --version "${version}"
+
+    echo "--- :helm: Pushing helm chart to ghcr.io"
+    helm push "dist/agent-stack-k8s-${version}.tgz" "${helm_repo_ghcr}"
+fi
 
 buildkite-agent annotate --style success --append <<EOF
 ### Helm Chart
 ----------------------------------------------------
 | Version    | Image                               |
 |------------|-------------------------------------|
-| ${version} | ${helm_image}                       |
+| ${version} | ${helm_repo_pecr:-}                 |
+|            | ${helm_repo_ghcr:-}                 |
 ----------------------------------------------------
 EOF
